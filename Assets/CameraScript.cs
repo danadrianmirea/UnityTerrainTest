26cd43e218bd66bac1ab25dd74f6db35feaa9b49
adrian mirea <dan_adrian_mirea@yahoo.com>
2024-01-29 03:08:48 +0200

    first commit

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 offset;// = new Vector3(0, 5, -10);
+    private Vector3 rotationOffset;
+
+
+    private float turnSpeed = 0.125f;
+
+    // camera is a child of the player gameobject now so, for now, this script does nothing
+    // will update it at some point maybe to to some fancy juicy stuff using smooth, lerp, etc
+    // and maybe turn it more into a 3rd person look around camera 
+
+    // Use this for initialization
+    void Start () {
+		//offset = transform.position - target.position;
+        //rotationOffset = transform.rotation;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        //transform.position = target.position + offset;
+        //transform.rotation = Quaternion.Euler(transform.eulerAngles.x, target.eulerAngles.y, transform.eulerAngles.z);
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, turnSpeed); //target.rotation; //Quaternion.Slerp(transform.rotation, target.rotation, Time.deltaTime * turnSpeed);
+        //transform.LookAt(target);
+
+
+        //Vector3 desiredPosition = target.position + target.rotation * offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, turnSpeed);
+        //transform.position = target.position + offset;
+
+        //Quaternion desiredrotation = target.rotation * Quaternion.Euler(rotationOffset);
+        //Quaternion smoothedrotation = Quaternion.Lerp(transform.rotation, desiredrotation, turnSpeed);
+        //transform.rotation = smoothedrotation;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private CharacterController controller;
+    private Rigidbody rb;
+
+    private float speed = 10.0f;
+    private float rotationSpeed = 3.0f;
+    private float jumpSpeed = 100.0f;
+
+    // Use this for initialization
+    void Start () {
+		//controller = GetComponent<CharacterController>();
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //Vector3 rightPos = new Vector3(3, 1.5f, transform.position.z);
+        //Vector3 leftPos = new Vector3(-3, 1.5f, transform.position.z);
+        //Vector3 middlePos = new Vector3(0, 1.5f, transform.position.z);
+
+        //if (controller.isGrounded) 
+        //{
+        if (Input.GetKey(KeyCode.W))
+        {
+            //rb.AddForce( new Vector3(1.0f, 0.0f, 0.0f) * speed * Time.deltaTime);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -rotationSpeed, 0));
+            //transform.Translate(-Vector3.right * Time.deltaTime * speed);
+        }
+        else if(Input.GetKey(KeyCode.D)) 
+        {
+            
+            transform.Rotate(new Vector3(0, rotationSpeed, 0));
+            //transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        //}
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            //Debug.Log("Am facut combinatia");
+            rb.AddForce(new Vector3(0.0f, jumpSpeed, 0.0f) * speed * Time.deltaTime);
+        }
+
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // transform.position = Vector3.MoveTowards(transform.position, leftPos, step);
+
+
+    }
+}
